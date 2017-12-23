8dd613f432c0e731f7960260cf703588e1123a92
Roberto Prato <roberto.prato@gmail.com>
2017-12-23 12:35:33 +0100

    allow setting of custom parameters in a  Request

--- src/Traits/ConnectApiRequestTrait.php
+++ src/Traits/ConnectApiRequestTrait.php
@@ -11,26 +11,33 @@ trait ConnectApiRequestTrait
         throw new Exception("associatedModel is not set for this request");
     }
     
- 
+    /**
+     * returning this to true automaticall adds   the following parameters to the request  'page' => 'integer','per_page' => 'integer',
+     *
+     * @return boolean
+     */
+  
     public function getIsPaginated()
     {
         return false;
     }
 
     /**
-     * return an array with the parameters for this request
+     * return an array with the parameters for this request 
      *
      * @return array
      */
-    public function parameters()
+    public  function parameters()
     {
         $params = array();
         $rules = $this->rules();
 
-        if (isset($this->params)) {
-            $params = array_merge($rules, $this->params);
-        } else {
-            $params = array_merge($rules, $params);
+        if(isset($this->params)) {
+            $params = array_merge($rules, $this->params); 
+        }
+        else 
+        {
+            $params = array_merge($rules, $params);  
         }
         return $params;
     }
